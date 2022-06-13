5f3a86126ce609496eb95a47676627f656a876da
Felioxx <102677497+Felioxx@users.noreply.github.com>
2022-06-13 13:42:56 +0200

    Update seite1.js

--- seite1.js
+++ seite1.js
@@ -1,4 +1,4 @@
- // Leaflet initialisieren
+// Leaflet initialisieren
 var map = L.map('map', { drawControl: true }).setView([51.975, 7.61], 13);
 // OpenStreetMap tile layer hinzufügen
 var osmLayer = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -14,33 +14,39 @@ var drawControl = new L.Control.Draw({
     }
 });
 map.addControl(drawControl);
- 
- 
- 
- // gezeichnetes Polygon initialisieren
-    map.on(L.Draw.Event.CREATED, function (event) {
-        var layer = event.layer;
-        drawnItems.addLayer(layer);
-        updateText();
-    });
-    map.on("draw:edited", function (event) {
-        updateText();
-    });
 
-    var gezeichnetesPolygon = [];
-    /**
-    * generiert ein GeoJSON aus dem gezeichneten Polygon und speichert die Koordinaten in einem Array
-    * und extrahiert nach dem Zeichnen die im Polygon enthaltenen Haltestellen
-    */
-    function updateText() {
 
-        // to convert L.featureGroup to GeoJSON FeatureCollection
-        document.getElementById("geojsontextarea").value = JSON.stringify(drawnItems.toGeoJSON());
-        //console.log(drawnItems.toGeoJSON());
-        var gezeichnetesGeojson = drawnItems.toGeoJSON();
-        gezeichnetesGeojson.features.forEach(element1 => {
-            element1.geometry.coordinates.forEach(element2 => {
-                gezeichnetesPolygon.push(element2);
-            })
+
+// gezeichnetes Polygon initialisieren
+map.on(L.Draw.Event.CREATED, function (event) {
+    var layer = event.layer;
+    drawnItems.addLayer(layer);
+    updateText();
+});
+map.on("draw:edited", function (event) {
+    updateText();
+});
+
+var gezeichnetesPolygon = [];
+var coords = [];
+/**
+* generiert ein GeoJSON aus dem gezeichneten Polygon und speichert die Koordinaten in einem Array
+* und extrahiert nach dem Zeichnen die im Polygon enthaltenen Haltestellen
+*/
+function updateText() {
+
+    // to convert L.featureGroup to GeoJSON FeatureCollection
+    document.getElementById("geojsontextarea").value = JSON.stringify(drawnItems.toGeoJSON());
+    //console.log(drawnItems.toGeoJSON());
+    var gezeichnetesGeojson = drawnItems.toGeoJSON();
+    gezeichnetesGeojson.features.forEach(element1 => {
+        element1.geometry.coordinates.forEach(element2 => {
+            gezeichnetesPolygon.push(element2);
         })
-    }
+    })
+}
+
+element.addEventListener("click", function () {
+    let eingabe = document.getElementById("textfeld").value;
+    console.log(eingabe, gezeichnetesPolygon);
+})
