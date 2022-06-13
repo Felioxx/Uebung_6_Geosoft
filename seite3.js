928ef6a0f039ae2b71098b320ade806c522ae65c
Felioxx <102677497+Felioxx@users.noreply.github.com>
2022-06-13 13:43:09 +0200

    Create seite3.js

---
+++ seite3.js
@@ -0,0 +1,6 @@
+element.addEventListener("click", function () {
+    let eingabe = document.getElementById("textfeld").value;
+    console.log(eingabe);
+})
+
+// TODO: Autovervollständigung implementieren
